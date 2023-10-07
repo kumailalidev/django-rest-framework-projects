44ebebab720d498019b159ef7735443c1a8f12fa
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-07 19:53:14 +0500

    P01-Created Author and Book model

--- 01-Books-API/books/models.py
+++ 01-Books-API/books/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """
+    Model for book author
+    """
+
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Model for book
+    """
+
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=True)  # optional
+    authors = models.ManyToManyField(Author, related_name="books")
+    edition = models.IntegerField()
+    isbn = models.CharField(max_length=13)
+    publication_date = models.DateField()  # release date
+
+    def __str__(self):
+        return self.title
