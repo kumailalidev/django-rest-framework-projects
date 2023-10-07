9ade33d30c4364d8e5317a62cb828edb1e9c1fbf
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-07 20:19:51 +0500

    P01-Created Author and Book list and detail api views

--- 01-Books-API/books/urls.py
+++ 01-Books-API/books/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import BookListView, BookDetailView, AuthorListView
+from .views import BookListAPIView, BookDetailAPIView, AuthorListAPIView
 
 urlpatterns = [
-    path("", BookListView.as_view(), name="book_list"),
-    path("book/<int:pk>/", BookDetailView.as_view(), name="book_detail"),
-    path("authors/", AuthorListView.as_view(), name="author_list"),
+    path("", BookListAPIView.as_view(), name="book_list"),
+    path("book/<int:pk>/", BookDetailAPIView.as_view(), name="book_detail"),
+    path("authors/", AuthorListAPIView.as_view(), name="author_list"),
 ]
--- 01-Books-API/books/views.py
+++ 01-Books-API/books/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+
+
+class AuthorListAPIView(generics.ListAPIView):
+    """
+    Author list view; Only HTTP GET request is allowed
+    """
+
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all()
+
+
+class BookListAPIView(generics.ListAPIView):
+    """
+    Book list view; Only HTTP GET request is allowed
+    """
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailAPIView(generics.RetrieveAPIView):
+    """
+    Book detail view; Only HTTP GET request in allowed
+    """
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
