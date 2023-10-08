34e294a633d4f13605667f8d9ae3c4f9fa8dc561
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-08 19:20:18 +0500

    P02-Created views for api endpoints

--- 02-Todo-API/todos/urls.py
+++ 02-Todo-API/todos/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import TodoList, TodoDetail
 
 urlpatterns = [
-    path("/todos/", TodoList.as_view(), name="todos_list"),
-    path("/todo/<int:pk>/", TodoDetail.as_view(), name="todos_detail"),
+    path("todos/", TodoList.as_view(), name="todos_list"),
+    path("todo/<int:pk>/", TodoDetail.as_view(), name="todos_detail"),
 ]
--- 02-Todo-API/todos/views.py
+++ 02-Todo-API/todos/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoList(generics.ListCreateAPIView):
+    """
+    List view for todos
+    Allows only GET and POST HTTP methods.
+    """
+
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class TodoDetail(generics.RetrieveUpdateAPIView):
+    """
+    Detail view for todo object.
+    Allows only GET and PATCH HTTP methods
+    """
+
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
