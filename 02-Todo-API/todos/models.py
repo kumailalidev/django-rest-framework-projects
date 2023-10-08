cdd5ae0d2443a4c8e0ce976ca07a8b86f9f46fe7
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-08 19:02:07 +0500

    P02-Created Todo model

--- 02-Todo-API/todos/models.py
+++ 02-Todo-API/todos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    """
+    Model for Todo
+    """
+
+    todo = models.CharField(max_length=250)
+    completed = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.todo
