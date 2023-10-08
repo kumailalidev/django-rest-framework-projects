1b284e54f05dbb67ec5ec6f3be6d7e03afb3fd42
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-10-08 20:33:52 +0500

    P02-Adding filtering based on URL parameters

--- 02-Todo-API/todos/views.py
+++ 02-Todo-API/todos/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from .models import Todo
 from .serializers import TodoSerializer
+
+from distutils.util import strtobool
 
 
 class TodoList(generics.ListCreateAPIView):
@@ -12,6 +15,33 @@ class TodoList(generics.ListCreateAPIView):
 
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+    # Overriding list method of view class
+    def list(self, request):
+        queryset = self.get_queryset()
+
+        # Getting URL parameters using 'self.request.get_query_params'
+        completed_param = request.query_params.get("completed")
+        sort_by = request.query_params.get("sort_by")
+
+        # Converting completed_param value to a Boolean value (with a try-except block)
+        if completed_param is not None:
+            try:
+                completed = bool(strtobool(completed_param.lower()))
+            except (ValueError, AttributeError):
+                completed = None
+        else:
+            completed = None
+
+        # Applying filters based on URL parameters
+        if completed is not None:
+            queryset = queryset.filter(completed=completed)
+
+        if sort_by == "date_updated":
+            queryset = queryset.order_by("date_updated")
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class TodoDetail(generics.RetrieveUpdateAPIView):
